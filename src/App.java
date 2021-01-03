79d525e8e13b369be04cf9e5ba63e32c9beca21b
Moss <NA>
2021-01-03 15:03:17 -0500

    adding driver application

---
+++ src/App.java
@@ -0,0 +1,9 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Car myFerrari = new Car("Ferrari","488"); 
+        myFerrari.moveForward();
+        myFerrari.brake();
+    }
+}
