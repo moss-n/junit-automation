b68a8369dfbb16aa7bddd2551a18adad2fb351ee
Moss <NA>
2021-01-07 21:04:48 -0500

    updating cartest

--- src/CarTest.java
+++ src/CarTest.java
@@ -11,7 +11,7 @@ public class CarTest {
     @Test
     public void testMake(){
         System.out.println("Running Test 1!");
-        Assertions.assertEquals(make, testCar.getMake(),"Expected vehicle make did not match actual vehicle make"); 
+        Assertions.assertEquals(make, "HONDAA","Expected vehicle make did not match actual vehicle make"); 
     }
 
     @Test 
